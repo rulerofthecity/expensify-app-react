524f1142cfb044c538b0f57564b1927c744550b0
Nadeem <rulerofthecity@gmail.com>
2020-11-18 19:56:26 +0530

    StartSetExpenses coming from DB done

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,5 +1,6 @@
 import uuid from "uuid";
 import database from "../firebase/firebase";
+import expenses from "../reducers/expenses";
 
 // ADD_EXPENSE
 export const addExpense = (expense) => ({
@@ -42,3 +43,24 @@ export const editExpense = (id, updates) => ({
   id,
   updates,
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+  type: "SET_EXPENSES",
+  expenses,
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database
+      .ref("expenses")
+      .once("value")
+      .then((snapshot) => {
+        const expenses = [];
+        snapshot.forEach((childSnapshot) => {
+          expenses.push({ id: childSnapshot.key, ...childSnapshot.val() });
+        });
+        dispatch(setExpenses(expenses));
+      });
+  };
+};
