d893b4f4301c3bed95b0251e950643c5b5f61ecd
Nadeem <rulerofthecity@gmail.com>
2020-11-19 16:08:05 +0530

    Update expenses

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -37,12 +37,34 @@ export const removeExpense = ({ id } = {}) => ({
   id,
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database
+      .ref(`expenses/${id}`)
+      .remove()
+      .then(() => {
+        dispatch(removeExpense({ id }));
+      });
+  };
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: "EDIT_EXPENSE",
   id,
   updates,
 });
+
+export const startEditExpense = (id, updates) => {
+  return (dispatch) => {
+    return database
+      .ref(`expenses/${id}`)
+      .update(updates)
+      .then(() => {
+        dispatch(editExpense(id, updates));
+      });
+  };
+};
 
 // SET_EXPENSES
 export const setExpenses = (expenses) => ({
@@ -61,17 +83,6 @@ export const startSetExpenses = () => {
           expenses.push({ id: childSnapshot.key, ...childSnapshot.val() });
         });
         dispatch(setExpenses(expenses));
-      });
-  };
-};
-
-export const startRemoveExpense = ({ id } = {}) => {
-  return (dispatch) => {
-    return database
-      .ref(`expenses/${id}`)
-      .remove()
-      .then(() => {
-        dispatch(removeExpense({ id }));
       });
   };
 };
